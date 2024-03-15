fec365838bc180c622764641738a75e91231f0b9
FakeNerfKaiZer <bloxdude101@gmail.com>
2024-03-15 14:32:33 +0000

    first commit

---
+++ src/musicapp/MusicApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package musicapp;
+
+public class MusicApp {
+    public static void main(String[] args) {
+        // Create and display the GUI
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MusicGUI().setVisible(true);
+            }
+        });
+    }
+}
+
